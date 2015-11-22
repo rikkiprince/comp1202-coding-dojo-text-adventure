59c246886588e206a5d2d18a6726fa51c470c1f1
Rikki Prince <rfp@ecs.soton.ac.uk>
2015-11-22 23:02:41 +0000

    Early input.

--- 2015/TextAdventure.java
+++ 2015/TextAdventure.java
@@ -1,7 +1,24 @@
+import java.util.*;
+
 public class TextAdventure {
 	
 	public static void main(String[] args) {
-		
+		TextAdventure textAdventure = new TextAdventure();
+		textAdventure.play();
 	}
 
+	private Scanner in;
+
+	public TextAdventure() {
+		this.in = new Scanner(System.in);
+	}
+
+	private void play() {
+		String input;
+
+		System.out.print("> ");
+		input = this.in.nextLine();
+
+		System.out.println(input);
+	}
 }
