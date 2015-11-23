bc5c2a5526d6d035ee5000a6e8271d6ac6815e14
Patrick McSweeney <mcsweeney.patrick@gmail.com>
2015-11-23 11:32:06 +0000

    rooms have exits

--- 2015/Room.java
+++ 2015/Room.java
@@ -1,4 +1,10 @@
 public class Room {
+	Map<String,Room> exits = new HashMap<String,Door>();	
+	
 
-
+	public void setExit(String direction, Room room)
+	{
+		Door door = new Door(this, room);
+		this.exits.put(direction, door);
+	}
 }
