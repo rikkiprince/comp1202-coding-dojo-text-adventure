680fec7f7e3d837b1f7c5a5b6f4997dfc92dc98d
Rikki Prince <rfp@ecs.soton.ac.uk>
2015-11-24 16:24:24 +0000

    Updated Item with getters and setters.

--- 2015/Item.java
+++ 2015/Item.java
@@ -1,11 +1,27 @@
 public class Item {
 	private String name;
+	private Integer size;
 
-	public Item(String name) {
+	public Item(String name, Integer size) {
 		this.name = name;
+		this.size = size;
 	}
 
 	public String toString() {
 		return this.name;
+	}
+
+	public Integer getSize() {
+		return this.size;
+	}
+	public void setSize(Integer newSize) {
+		this.size = newSize;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String newName) {
+		this.name = newName;
 	}
 }
